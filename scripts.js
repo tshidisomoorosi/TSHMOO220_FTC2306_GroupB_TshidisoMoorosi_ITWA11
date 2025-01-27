5566c27c113c2109d4ecd7fb87f880e5718b2a60
tshidiso-codespace <tshidisomoorosi@gmail.com>
2025-01-27 20:01:49 +0200

    edited hard code

--- scripts.js
+++ scripts.js
@@ -1,33 +1,23 @@
-const root_1 = document.querySelector('[data-key="order1"]')
-// const biscuits_1 = document.querySelector('[data-biscuits="10"]')
-// const donuts_1 = document.querySelector('[data-donuts="13"]')
-// const pancakes_1 = document.querySelector('[data-pancakes="0"]')
-// const status_1 = document.querySelector('[data-delivered="false"]')
+const root_1 = document.querySelector('[data-key="order1"]');
 
-const root_2 = document.querySelector('[data-key="order2"]')
-// const biscuits_2 = document.querySelector('[data-biscuits="5"]')
-// const donuts_2 = document.querySelector('[data-donuts="0"]')
-// const pancakes_2 = document.querySelector('[data-pancakes="2"]')
-// const status_2 = document.querySelector('[data-delivered="false"]')
+const root_2 = document.querySelector('[data-key="order2"]');
 
-const root_3 = document.querySelector('[data-key="order3"]')
-// const biscuits_3 = document.querySelector('[data-biscuits="12"]')
-// const donuts_3 = document.querySelector('[data-donuts="11"]')
-// const pancakes_3 = document.querySelector('[data-pancakes="15"]')
-// const status_3 = document.querySelector('[data-delivered="true"]')
+const root_3 = document.querySelector('[data-key="order3"]');
 
+root_1.firstElementChild.children[1].textContent = root_1.dataset.biscuits;
+root_1.children[1].children[1].textContent = root_1.dataset.donuts;
+root_1.children[2].children[1].textContent = root_1.dataset.pancakes;
+root_1.lastElementChild.children[1].textContent =
+  root_1.dataset.delivered === "true" ? "Delivered" : "Pending";
 
-root_1.firstElementChild.children[1].textContent = "10";
-root_1.children[1].children[1].textContent = "13";
-root_1.children[2].children[1].textContent = "0";
-root_1.lastElementChild.children[1].textContent = "false";
+root_2.firstElementChild.children[1].textContent = root_2.dataset.biscuits;
+root_2.children[1].children[1].textContent = root_2.dataset.donuts;
+root_2.children[2].children[1].textContent = root_2.dataset.pancakes;
+root_2.lastElementChild.children[1].textContent =
+  root_2.dataset.delivered === "true" ? "Delivered" : "Pending";
 
-root_2.firstElementChild.children[1].textContent = "5"
-root_2.children[1].children[1].textContent = "0";
-root_2.children[2].children[1].textContent = "2";
-root_2.lastElementChild.children[1].textContent = "false";
-
-root_3.firstElementChild.children[1].textContent = "12"
-root_3.children[1].children[1].textContent = "11";
-root_3.children[2].children[1].textContent = "15";
-root_3.lastElementChild.children[1].textContent = "true";
+root_3.firstElementChild.children[1].textContent = root_3.dataset.biscuits;
+root_3.children[1].children[1].textContent = root_3.dataset.donuts;
+root_3.children[2].children[1].textContent = root_3.dataset.pancakes;
+root_3.lastElementChild.children[1].textContent =
+  root_3.dataset.delivered === "true" ? "Delivered" : "Pending";
